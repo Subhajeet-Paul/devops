468f3483fc35aade7cdb2bc0a82f42a6cd17b02e
Subhajeet-Paul <subhajeetpaul28@gmail.com>
2026-03-15 22:53:55 +0530

    pipeline code testing with pipeline.yaml

---
+++ devops-pipeline/index.js
@@ -0,0 +1,1 @@
+console.log("hello, i am learning ci/cd pipelines using github actions ");
---
+++ devops-pipeline/test.js
@@ -0,0 +1,5 @@
+console.log("script running... ");
+setTimeout(() => {
+  console.log("execution complete...");
+  //   window.location.href = "https://www.google.com";
+},3000)
